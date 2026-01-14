f75a6ed1f966e1a679da1d71a8cb925ea42410f5
SuslovQA <evgenijsuslov08683@gmail.com>
2026-01-14 14:09:24 +0500

    Update DeliveryTest.java

--- src/test/java/test/DeliveryTest.java
+++ src/test/java/test/DeliveryTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
